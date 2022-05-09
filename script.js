866b4429bbd3a3664380863d1a5b4aa1d39ca6d1
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-05-09 17:48:55 -0300

    Fix: fixing delete button that wasn't working

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ const form = document.querySelector("#form");
 const inputTransactionName = document.querySelector("#text");
 const inputTransactionAmount = document.querySelector("#amount");
 
-const dummyTransactions = [
+let dummyTransactions = [
   { id: 1, name: "Bolo de Brigadeiro", amount: -20 },
   { id: 2, name: "Salario", amount: 300 },
   { id: 3, name: "Torta de Frango", amount: -10 },
   { id: 4, name: "Violão", amount: 150 },
 ];
 
-const removeTransaction = (ID) => {
-  dummyTransactions.filter((transaction) => transaction.id !== ID);
-  console.log(dummyTransactions);
-};
+const removeTransaction = ID => {
+  dummyTransactions = dummyTransactions.filter(transaction => transaction.id !== ID)
+  init()
+}
 
 const addTransactionIntoDOM = (transaction) => {
   const operator = transaction.amount < 0 ? "-" : "+";
